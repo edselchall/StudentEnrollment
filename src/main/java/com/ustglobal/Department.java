20ded77e7655dd5cb5ed88df760020651230bdc0
U66324 <U66324@F702LLAX.ustr.com>
2018-04-27 11:34:43 -0500

    Created Department POJO & Instructor POJO

---
+++ src/main/java/com/ustglobal/Department.java
@@ -0,0 +1,28 @@
+package com.ustglobal;
+
+public class Department {
+	int Dept_Id;
+	String Dept_Name;
+	String College;
+	
+	public int getDept_Id() {
+		return Dept_Id;
+	}
+	public void setDept_Id(int dept_Id) {
+		Dept_Id = dept_Id;
+	}
+	public String getDept_Name() {
+		return Dept_Name;
+	}
+	public void setDept_Name(String dept_Name) {
+		Dept_Name = dept_Name;
+	}
+	public String getCollege() {
+		return College;
+	}
+	public void setCollege(String college) {
+		College = college;
+	}
+	
+
+}
---
+++ src/main/java/com/ustglobal/Instructor.java
@@ -0,0 +1,49 @@
+package com.ustglobal;
+
+
+public class Instructor {
+	String Last_name;
+	String First_name;
+	int Dept_Id;
+	String Office;
+	String PhoneNo;
+	String email;
+	
+	public String getLast_name() {
+		return Last_name;
+	}
+	public void setLast_name(String last_name) {
+		Last_name = last_name;
+	}
+	public String getFirst_name() {
+		return First_name;
+	}
+	public void setFirst_name(String first_name) {
+		First_name = first_name;
+	}
+	public int getDept_Id() {
+		return Dept_Id;
+	}
+	public void setDept_Id(int dept_Id) {
+		Dept_Id = dept_Id;
+	}
+	public String getOffice() {
+		return Office;
+	}
+	public void setOffice(String office) {
+		Office = office;
+	}
+	public String getPhoneNo() {
+		return PhoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		PhoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
