a2d8a61423adfdb3d1df75669ef106968e0b1313
U66324 <U66324@F702LLAX.ustr.com>
2018-04-30 14:38:29 -0500

    Created AdminService java class

---
+++ src/main/java/com/ustglobal/StudentEnrollment/services/AdminService.java
@@ -0,0 +1,28 @@
+package com.ustglobal.StudentEnrollment.services;
+
+import com.ustglobal.Course;
+
+public class AdminService {
+
+	public void createCourse(Course mockCourse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Course getCourse(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void DropCourse(Course mockCourse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void PrepareTermSchedule() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
