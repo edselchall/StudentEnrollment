cbec62c3477bbbae4918c65c0b99a943d28d203f
TyDell Miller <U65626@F701LLAX.ustr.com>
2018-05-02 13:28:06 -0500

    change resources to assets

--- src/main/java/com/ustglobal/StudentEnrollment/config/MvcConfiguration.java
+++ src/main/java/com/ustglobal/StudentEnrollment/config/MvcConfiguration.java
@@ -32,7 +32,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
 	}
 	
 	@Bean
